44413b2009ae969e0578ef99a6e71bc042d3bba3
AdrakPro <adrak99.adrak99@gmail.com>
2023-10-24 12:01:01 +0200

    Skeleton

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/vitest/**/*.test.{js,ts}'],
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/vitest/setupTests.js'],
+    reporters: ['verbose'],
+    restoreMocks: true
+  }
+});
