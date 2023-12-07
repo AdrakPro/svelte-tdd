b7f20b1c9ecd0ffd0502d30e0e76a0f7dd3c231a
AdrakPro <adrak99.adrak99@gmail.com>
2023-12-07 22:36:58 +0100

    Chapter 13

---
+++ features/support/hooks.mjs
@@ -0,0 +1,9 @@
+import { Before, After } from '@cucumber/cucumber';
+Before(async function () {
+  await this.launchServer();
+  await this.launchBrowser();
+});
+After(async function () {
+  await this.killServer();
+  await this.closeBrowser();
+});
